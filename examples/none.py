80edf1db5c3f5bd5aeef82fe2c2d4bc0722dad7a
Daniil688 <cherkdan80@gmail.com>
2024-07-21 15:38:26 +0300

    3.38

--- examples/none.py
+++ examples/none.py
@@ -3,8 +3,35 @@ This driver does not do any action.
 """
 from rose.common import obstacles, actions  # NOQA
 
-driver_name = "No Driver"
+driver_name = "Avraham"
 
 
 def drive(world):
-    return actions.NONE
+    x = world.car.x
+    y = world.car.y
+    your_way = [x//3 * 3, x//3 * 3 + 1, x//3 * 3 + 2]
+    obstacle = world.get((x, y - 1))
+    match obstacle:
+        case obstacles.PENGUIN:
+            return actions.PICKUP
+        case obstacles.CRACK:
+            return actions.JUMP
+        case obstacles.WATER:
+            return actions.BRAKE
+        case obstacles.NONE:
+            return prediction(world, x, y, your_way)
+        case _:
+            if x - 1 in your_way:
+                return actions.LEFT
+            return actions.RIGHT
+
+
+def prediction(world, x, y, your_way):
+    if obstacles.PENGUIN in (world.get((x - 1, y - 2)), world.get((x - 1, y - 2))) and x - 1 in your_way:
+        return actions.LEFT
+    elif obstacles.PENGUIN in (world.get((x, y - 2)), world.get((x, y - 2))):
+        return actions.NONE
+    elif obstacles.PENGUIN in (world.get((x + 1, y - 2)), world.get((x + 1, y - 2))) and x + 1 in your_way:
+        return actions.RIGHT
+    else:
+        return actions.NONE
